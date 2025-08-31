f4c4dc0e81ca78046f25ab15a2f0c51df805948f
evaine <enoxy1998@gmail.com>
2025-08-31 04:42:29 +0100

    ci: CI to  green

--- backend/tests/test_health.py
+++ backend/tests/test_health.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from app.main import create_app
 from fastapi.testclient import TestClient
 
@@ -5,6 +6,5 @@ from fastapi.testclient import TestClient
 def test_health():
     client = TestClient(create_app())
     r = client.get("/health")
-    assert r.status_code == 200
+    assert r.status_code == HTTPStatus.OK
     assert r.json() == {"ok": True}
-
